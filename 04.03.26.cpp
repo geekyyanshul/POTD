0f85b3a44ba1486408738e2d2d96fa161a9ec2ca
Rustydonut <157903412+geekyyanshul@users.noreply.github.com>
2024-03-04 20:33:14 +0530

    Create 04.03.26.cpp
    POTD - Swap the array elements

---
+++ 04.03.26.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+  public:
+    void swapElements(int arr[], int n)
+    {
+        
+         for(int i=2;i<n;i++)
+        {
+            int temp=arr[i];
+            arr[i]=arr[i-2];
+            arr[i-2]=temp;
+        }
+        
+    }
+};
+
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int n;
+	    cin >> n;
+	    
+	    int arr[n];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr[i];
+	    }
+        Solution obj;
+       
+	    obj.swapElements(arr, n);
+	  
+	    for(int i = 0;i<n;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
+}
