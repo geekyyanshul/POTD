07cc27704634ee1ad4a9ef344d60f46ba40830a4
Rustydonut <157903412+geekyyanshul@users.noreply.github.com>
2024-05-31 02:47:09 +0530

    30.05.2024
    GFG POTD

---
+++ Swap two nibbles in a byte.java
@@ -0,0 +1,28 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.swapNibbles(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    static int swapNibbles(int n) {
+          int second = n&15; 
+        int first = n>>4; 
+        second = second<<4; 
+        return second|first;
+    }
+}
