3c140ee05179efbe9c5e19c168ff0c634d4bc6b6
Rustydonut <157903412+geekyyanshul@users.noreply.github.com>
2024-09-11 09:41:45 +0530

    11.09.2024
    GFG POTD

---
+++ September2024/11.Minimum Cost of ropes.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+
+        while (t-- > 0) {
+            String[] input = br.readLine().split(" ");
+            long[] a = new long[input.length];
+
+            for (int i = 0; i < input.length; i++) {
+                a[i] = Long.parseLong(input[i]);
+            }
+
+            Solution ob = new Solution();
+            System.out.println(ob.minCost(a));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+ 
+class Solution {
+     
+    public long minCost(long[] arr) {
+        
+        Queue<Long> pq=new PriorityQueue<>();
+        long total=0;
+        for(long i:arr) pq.add(i);
+        while(pq.size()>1){
+            long a=pq.poll(),b=pq.poll();
+            total+=(a+b);
+            pq.add(a+b);
+        }
+        return total;
+    }
+}
