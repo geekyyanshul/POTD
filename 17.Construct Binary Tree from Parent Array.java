13ce8c824418f7a2f2558b502fc0c5f77e62fa81
Rustydonut <157903412+geekyyanshul@users.noreply.github.com>
2025-05-19 23:48:29 +0530

    Update 17.Construct Binary Tree from Parent Array.java

--- 17.Construct Binary Tree from Parent Array.java
+++ 17.Construct Binary Tree from Parent Array.java
@@ -19,9 +19,9 @@ class CreateTree {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        while (t-- > 0) {
+        whle (t-- > 0) {
             int n = sc.nextInt();
-            int arr[] = new int[n];
+              int arr[] = new int[n];
 
             for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
